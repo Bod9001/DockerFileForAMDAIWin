5e6ef0b6cc3cca729b95086fe828bbdf0b576e09
Bod9001 <Bod9001@gmail.com>
2024-07-22 00:56:04 +0100

    fix

--- automatic1111_modified_install_script.sh
+++ automatic1111_modified_install_script.sh
@@ -2,4 +2,4 @@ can_run_as_root=0
 
 mkdir stable-diffusion-webui
 cd stable-diffusion-webui
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git "${clone_dir}"
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git stable-diffusion-webui
